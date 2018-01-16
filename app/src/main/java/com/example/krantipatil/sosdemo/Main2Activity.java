3af62e099c2dd2f5b6dad8f0fe6810d721f43be2
Rohan2210 <35292795+Rohan2210@users.noreply.github.com>
2018-01-16 22:49:06 +0530

    megng both...see for mainfest still showing for old

---
+++ app/src/main/java/com/example/krantipatil/sosdemo/Main2Activity.java
@@ -0,0 +1,88 @@
+package com.example.krantipatil.sosdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main2Activity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_profile)
+        {
+            Intent profile = new Intent(Main2Activity.this,profile.class);
+            startActivity(profile);
+        }
+        else if (id == R.id.nav_add_contact)
+        {
+            Intent contact = new Intent(Main2Activity.this,Emeregency_contacts.class);
+            startActivity(contact);
+
+        }
+        else if (id == R.id.nav_add_locations)
+        {
+            Intent location = new Intent(Main2Activity.this,Add_location.class);
+            startActivity(location);
+
+        } else if (id == R.id.nav_logout)
+        {
+            Intent logout = new Intent(Main2Activity.this,home.class);
+            startActivity(logout);
+
+        }
+
+        else if (id == R.id.nav_share)
+        {
+
+        } else if (id == R.id.nav_send)
+        {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
