44775f8a4a1a313333b0f8b4582806a910b2d9eb
Rohan2210 <35292795+Rohan2210@users.noreply.github.com>
2018-01-17 14:57:20 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.gradle/4.1/fileHashes/fileHashes.bin
    #	.gradle/4.1/fileHashes/fileHashes.lock
    #	.gradle/4.1/javaCompile/classAnalysis.bin
    #	.gradle/4.1/javaCompile/javaCompile.lock
    #	.gradle/4.1/javaCompile/taskHistory.bin
    #	.gradle/4.1/taskHistory/fileSnapshots.bin
    #	.gradle/4.1/taskHistory/taskHistory.bin
    #	.gradle/4.1/taskHistory/taskHistory.lock
    #	app/src/main/java/com/example/krantipatil/sosdemo/MainActivity.java
    #	app/src/main/java/com/example/krantipatil/sosdemo/Sign_in.java
    #	app/src/main/java/com/example/krantipatil/sosdemo/Sign_up.java
    #	app/src/main/res/layout/activity_add_location.xml
    #	app/src/main/res/layout/activity_emeregency_contacts.xml
    #	app/src/main/res/layout/activity_home.xml
    #	app/src/main/res/layout/activity_sign_in.xml
    #	app/src/main/res/layout/activity_sign_up.xml
    #	app/src/main/res/layout/content_main2.xml
    #	app/src/main/res/menu/activity_main2_drawer.xml

--- app/src/main/java/com/example/krantipatil/sosdemo/Main2Activity.java
+++ app/src/main/java/com/example/krantipatil/sosdemo/Main2Activity.java
@@ -1,6 +1,9 @@
 package com.example.krantipatil.sosdemo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,11 +11,19 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    public static boolean checker = true;
+    MediaPlayer mediaPlayer;
+    AudioManager am;
+    private Button call, message, siren;
+    private ImageView sos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,50 @@ public class Main2Activity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        init();
+    }
+
+    private void init() {
+        sos = findViewById(R.id.sos_img_button);
+        siren = findViewById(R.id.siren_button);
+        message = findViewById(R.id.message_button);
+        call = findViewById(R.id.call_button);
+        sos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Main2Activity.this, "sos", Toast.LENGTH_SHORT).show();
+            }
+        });
+        siren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checker == true) {
+                    am = (AudioManager) (getApplicationContext().getSystemService(Context.AUDIO_SERVICE));
+                    mediaPlayer = MediaPlayer.create(Main2Activity.this, R.raw.siren);
+                    mediaPlayer.setLooping(true);
+                    mediaPlayer.start();
+                    Toast.makeText(Main2Activity.this, "siren on", Toast.LENGTH_SHORT).show();
+                    checker = false;
+                } else {
+                    checker = true;
+                    mediaPlayer.stop();
+                    Toast.makeText(Main2Activity.this, "siren off", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Main2Activity.this, "meassage", Toast.LENGTH_SHORT).show();
+            }
+        });
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Main2Activity.this, "call", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
