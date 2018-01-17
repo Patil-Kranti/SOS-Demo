3b2caba6687e8bd1e26d00b044e85c75ddae6c5d
Kranti Patil <patilkranti03@gmail.com>
2018-01-17 16:06:21 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.gradle/4.1/fileHashes/fileHashes.bin
    #	.gradle/4.1/fileHashes/fileHashes.lock
    #	.gradle/4.1/javaCompile/classAnalysis.bin
    #	.gradle/4.1/javaCompile/javaCompile.lock
    #	.gradle/4.1/javaCompile/taskHistory.bin
    #	.gradle/4.1/javaCompile/taskJars.bin
    #	.gradle/4.1/taskHistory/fileSnapshots.bin
    #	.gradle/4.1/taskHistory/taskHistory.bin
    #	.gradle/4.1/taskHistory/taskHistory.lock
    #	.idea/libraries/com_android_support_design_26_1_0.xml
    #	.idea/libraries/com_android_support_recyclerview_v7_26_1_0.xml
    #	.idea/libraries/com_android_support_transition_26_1_0.xml
    #	.idea/vcs.xml
    #	app/src/main/java/com/example/krantipatil/sosdemo/Add_location.java
    #	app/src/main/java/com/example/krantipatil/sosdemo/Emeregency_contacts.java
    #	app/src/main/java/com/example/krantipatil/sosdemo/MainActivity.java
    #	app/src/main/java/com/example/krantipatil/sosdemo/Sign_in.java
    #	app/src/main/java/com/example/krantipatil/sosdemo/Sign_up.java
    #	app/src/main/java/com/example/krantipatil/sosdemo/home.java
    #	app/src/main/java/com/example/krantipatil/sosdemo/profile.java
    #	app/src/main/res/drawable/gradientbackground.xml
    #	app/src/main/res/drawable/ic_add_location_black_24dp.xml
    #	app/src/main/res/drawable/ic_chat_black_24dp.xml
    #	app/src/main/res/drawable/ic_contacts_black_24dp.xml
    #	app/src/main/res/drawable/ic_content_paste_black_24dp.xml
    #	app/src/main/res/drawable/ic_domain_black_24dp.xml
    #	app/src/main/res/drawable/ic_email_black_24dp.xml
    #	app/src/main/res/drawable/ic_feedback_black_24dp.xml
    #	app/src/main/res/drawable/ic_lock_black_24dp.xml
    #	app/src/main/res/drawable/ic_person_add_black_24dp.xml
    #	app/src/main/res/drawable/ic_person_black_24dp.xml
    #	app/src/main/res/drawable/ic_phone_android_black_24dp.xml
    #	app/src/main/res/drawable/ic_settings_applications_black_24dp.xml
    #	app/src/main/res/drawable/pro_name.xml
    #	app/src/main/res/layout/activity_add_location.xml
    #	app/src/main/res/layout/activity_emeregency_contacts.xml
    #	app/src/main/res/layout/activity_home.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/activity_profile.xml
    #	app/src/main/res/layout/activity_sign_in.xml
    #	app/src/main/res/layout/activity_sign_up.xml
    #	app/src/main/res/menu/list.xml

--- app/src/main/java/com/example/krantipatil/sosdemo/Emeregency_contacts.java
+++ app/src/main/java/com/example/krantipatil/sosdemo/Emeregency_contacts.java
@@ -1,13 +1,119 @@
 package com.example.krantipatil.sosdemo;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Emeregency_contacts extends AppCompatActivity {
+    public static final int PICK_CONTACT = 1;
+    private static final String TAG = "Emeregency_contacts";
+    public Uri uriContact;
+    private Button btnCall, btnMessage;
+    private TextView tvCallContact, tvCallNumber;
+    private String contactID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emeregency_contacts);
+        init();
+    }
+
+    private void init() {
+        tvCallContact = findViewById(R.id.tvCallContact);
+        tvCallNumber = findViewById(R.id.tvCallNumber);
+        btnCall = findViewById(R.id.add_emergency_call_button);
+        btnMessage = findViewById(R.id.add_emergency_message_button);
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickAContactNumber(v);
+
+            }
+        });
+    }
+
+    public void pickAContactNumber(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        if (reqCode == PICK_CONTACT && resultCode == RESULT_OK) {
+            Log.d(TAG, "Response: " + data.toString());
+            uriContact = data.getData();
+
+            retriveContactName();
+            retrieveContactNumber();
+
+        }
+    }
+
+    public void retrieveContactNumber() {
+
+        String contactNumber = null;
+
+        // getting contacts ID
+        Cursor cursorID = getContentResolver().query(uriContact,
+                new String[]{ContactsContract.Contacts._ID},
+                null, null, null);
+
+        if (cursorID.moveToFirst()) {
+
+            contactID = cursorID.getString(cursorID.getColumnIndex(ContactsContract.Contacts._ID));
+        }
+
+        cursorID.close();
+
+        Log.d(TAG, "Contact ID: " + contactID);
+
+        // Using the contact ID now we will get contact phone number
+        Cursor cursorPhone = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE + " = " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE,
+
+                new String[]{contactID},
+                null);
+
+        if (cursorPhone.moveToFirst()) {
+            contactNumber = cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+        }
+
+        cursorPhone.close();
+
+        tvCallNumber.setText("Contact Number:\n" + contactNumber);
+    }
+
+    public void retriveContactName() {
+
+        String contactName = null;
+
+        // querying contact data store
+        Cursor cursor = getContentResolver().query(uriContact, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+
+            // DISPLAY_NAME = The display name for the contact.
+            // HAS_PHONE_NUMBER =   An indicator of whether this contact has at least one phone number.
+
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        }
+
+        cursor.close();
+
+        tvCallContact.setText("Name:\n" + contactName);
+
     }
 }
+
